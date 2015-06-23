904484d89bd3bcee8230d5afa0922b6d98cd1c39
Greg Fitzgerald <greg@gregf.org>
2015-06-23 14:10:57 -0400

    Add release tools

---
+++ scripts/release.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -e
+
+RELEASE="v$1"
+CURRENT="$(git tag | tail -n1)"
+
+echo "Installing needed tools..."
+go get github.com/aktau/github-release
+
+echo "Creating Release"
+mkdir -p release
+go build podfetcher.go
+mv podfetcher release
+cp scripts/Makefile release
+cp examples/config.toml release
+cp examples/feeds release
+
+mv release podfetcher
+
+tar cfav podfetcher.tar.bz2 podfetcher
+rm -rf podfetcher
+
+LOG="$(git log --pretty=oneline --abbrev-commit "$CURRENT"..HEAD)"
+git tag "$RELEASE"
+git push origin "$RELEASE"
+github-release release \
+--user gregf \
+--repo podfetcher \
+--tag "$RELEASE" \
+--name "podfetcher" \
+--description "$LOG" \
+--file "podfetcher.tar.bz2"
