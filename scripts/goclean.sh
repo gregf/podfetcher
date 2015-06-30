93f3bbdabbb09e5e620eba0b4bc9abf71857039a
Greg Fitzgerald <greg@gregf.org>
2015-06-30 11:00:18 -0400

    Fix up goclean.sh

--- scripts/goclean.sh
+++ scripts/goclean.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-# The script does automatic checking on a Go package and its sub-packages,
-# including:
+# The script does automatic checking on a Go package and its sub-packages, including:
 # 1. gofmt         (http://golang.org/cmd/gofmt/)
 # 2. goimports     (https://github.com/bradfitz/goimports)
 # 3. golint        (https://github.com/golang/lint)
@@ -21,23 +20,19 @@ env GORACE="halt_on_error=1" go test -v -race ./...
 echo "mode: count" > profile.cov
 
 # Standard go tooling behavior is to ignore dirs with leading underscores.
-for dir in $(find . -maxdepth 10 -not -path './.git*' -not -path '*/_*' -type
-  d);
+for dir in $(find . -maxdepth 10 -not -path './Godeps*' -not -path './.git*' -not -path '*/_*' -type d);
 do
-  if ls $dir/*.go &> /dev/null; then
-      go test -covermode=count -coverprofile=$dir/profile.tmp $dir
-        if [ -f $dir/profile.tmp ]; then
-              cat $dir/profile.tmp | tail -n +2 >> profile.cov
-                  rm $dir/profile.tmp
-                    fi
-                  fi
-                done
-
-                go tool cover -func profile.cov
-
-                # To submit the test coverage result to coveralls.io,
-                # use goveralls (https://github.com/mattn/goveralls)
-                # goveralls -coverprofile=profile.cov -service=travis-ci
-        fi
+if ls $dir/*.go &> /dev/null; then
+  go test -covermode=count -coverprofile=$dir/profile.tmp $dir
+  if [ -f $dir/profile.tmp ]; then
+    cat $dir/profile.tmp | tail -n +2 >> profile.cov
+    rm $dir/profile.tmp
   fi
+fi
 done
+
+go tool cover -func profile.cov
+
+# To submit the test coverage result to coveralls.io,
+# use goveralls (https://github.com/mattn/goveralls)
+# goveralls -coverprofile=profile.cov -service=travis-ci
