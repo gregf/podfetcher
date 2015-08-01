9630d7fb8f63b4aab5cc7e75996d8f4fbfd98de1
Greg Fitzgerald <greg@gregf.org>
2015-08-01 15:30:36 -0400

    Remove got test for now, we don't have tests anyways

--- scripts/goclean.sh
+++ scripts/goclean.sh
@@ -17,29 +17,3 @@ echo "golint..."
 test -z "$(golint src | tee /dev/stderr)"
 echo "go vet..."
 go vet ./src/...
-echo "go test..."
-env GORACE="halt_on_error=1" go test -v -race
-
-# Run test coverage on each subdirectories and merge the coverage profile.
-
-echo "mode: count" > profile.cov
-
-# Standard go tooling behavior is to ignore dirs with leading underscores.
-for dir in $(find . -maxdepth 10 -not -path './Godeps*' -not -path './.git*' -not -path '*/_*' -type d);
-do
-if ls $dir/*.go &> /dev/null; then
-  echo "go test $dir..."
-  go test -covermode=count -coverprofile=$dir/profile.tmp $dir
-  if [ -f $dir/profile.tmp ]; then
-    cat $dir/profile.tmp | tail -n +2 >> profile.cov
-    rm $dir/profile.tmp
-  fi
-fi
-done
-
-echo "go tool..."
-go tool cover -func profile.cov
-
-# To submit the test coverage result to coveralls.io,
-# use goveralls (https://github.com/mattn/goveralls)
-# goveralls -coverprofile=profile.cov -service=travis-ci
